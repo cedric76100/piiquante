b30bf72343e8bbdab324c322c2ed24a0dcae4788
cedric massalon <c.massalon@gmail.com>
2022-11-09 10:06:41 +0100

    signin

--- back/controllers/auth.js
+++ back/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/users");
 exports.signup = (req, res, next) => {
+  //creer un nouvelle utilisateur en ce basant sur le modele
   const user = new User({
     email: req.body.email,
     password: req.body.password,
@@ -8,4 +9,16 @@ exports.signup = (req, res, next) => {
     .save()
     .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
     .catch((error) => res.status(400).json({ error }));
+}
+exports.signin = (req, res, next) => {
+  User.findOne({ 'email': req.body.email })
+    //user correspond a la reponse de la base de donnée qui renvoie l'utilisateur
+    .then((user) => {
+      // verifier si l'utilisateur existe sinon renvoyer une erreur au front
+      if (!user) {
+        return res.status(401).json({ error: "Utilisateur non trouvé !" });
+      }
+      //verifier que le mot de passe envoyé correspond au mot de passe dans la base de donnée
+    })
+    .catch((error) => res.status(500).json({ error }));
 }
--- back/routes/auth.js
+++ back/routes/auth.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const authCtrl = require("../controllers/auth");
 
 router.post("/signup", authCtrl.signup);
-
+router.post("/signin", authCtrl.signin);
 module.exports = router;
